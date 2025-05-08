92087bb8b3f698d089d7648793a70c4b1d0adf68
ylfm <lapu2023@outlook.com>
2025-05-08 12:49:19 +0800

    小卡片

--- src/views/pages/ProcuctPage.vue
+++ src/views/pages/ProcuctPage.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import HouseCard1 from "~/src/components/houseDetail/HouseCard1.vue";
+import Newsletter2 from "~/src/components/houseDetail/Newsletter2.vue";
+import Feature5 from "~/src/components/houseDetail/Feature5.vue";
 import { onMounted } from "vue";
 
 onMounted(() => {
@@ -12,6 +14,10 @@ onMounted(() => {
     <v-row class="flex-0" dense>
       <v-col cols="12" xl="4">
         <HouseCard1 />
+      </v-col>
+      <v-col cols="12" xl="4">
+        <Newsletter2 />
+        <Feature5 />
       </v-col>
     </v-row>
   </div>
