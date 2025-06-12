f28e9af7a1a2ed55a0fde8ea6cf2f626f8c45310
Andyiscpp <2298786941@qq.com>
2025-06-12 21:00:58 +0800

    头像（涉及页面较多）

--- src/stores/profileStore.ts
+++ src/stores/profileStore.ts
@@ -56,6 +56,13 @@ export const useProfileStore = defineStore({
     },
   }),
   actions: {
+    // 在 actions 中添加的用户头像更改
+    //-----------------------------------------------
+    updateAvatar(newAvatarUrl: string) {
+      this.user.avatarUrl = newAvatarUrl;
+      this.signon.avatarUrl = newAvatarUrl;
+      this.basic.avatar = newAvatarUrl;
+    },
     // 获取用户的 ID
     getUserId(): string {
       return this.user?.id || 'anonymous';
